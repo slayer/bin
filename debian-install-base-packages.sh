a7a1d12367fda397725dfd13277585b34e4a735d
Vladislav Moskovets <github@vlad.org.ua>
2013-05-30 04:08:13 +0300

    up

--- debian-install-base-packages.sh
+++ debian-install-base-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-BASE_PACKAGES="etckeeper tmux colordiff ctags vim dnsutils whois mtr-tiny curl pwgen whois stow dnsutils"
+BASE_PACKAGES="etckeeper tmux colordiff sudo ctags vim dnsutils whois mtr-tiny curl pwgen whois stow dnsutils"
 
 apt-get update
 apt-get --force-yes upgrade
@@ -8,3 +8,11 @@ locale-gen ru_RU.UTF-8 ru_UA.UTF-8
 apt-get -y install git-core
 
 apt-get -y install $BASE_PACKAGES
+
+echo "vlad ALL=NOPASSWD: ALL" >>/etc/sudoers
+
+sed -i 's/^VCS=.*$/VCS=git/' /etc/etckeeper/etckeeper.conf
+pushd /etc
+	etckeeper init && etckeeper commit "initial"
+popd
+
