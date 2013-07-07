7b0f28c68dacc28c757b4f6f5df8c7393787826b
Vlad <devvlad@gmail.com>
2013-07-07 21:53:08 +0300

    up

---
+++ debian-install-desktop.sh
@@ -0,0 +1,15 @@
+
+sudo apt-get install fonts-droid ttf-mscorefonts-installer
+
+
+
+sudo add-apt-repository ppa:tiheum/equinox
+sudo apt-get update && sudo apt-get install faenza-icon-theme
+
+
+sudo add-apt-repository ppa:nilarimogard/webupd8
+
+
+
+
+sudo apt-get install gnome-tweak-tool
