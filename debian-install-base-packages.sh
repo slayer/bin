b99e6e01904706ff1503f9e916cc87e24e57ff52
Vlad <devvlad@gmail.com>
2014-01-18 00:26:28 +0200

    up

--- debian-install-base-packages.sh
+++ debian-install-base-packages.sh
@@ -3,7 +3,7 @@
 BASE_PACKAGES="etckeeper tmux grc colordiff sudo ctags vim dnsutils whois mtr-tiny curl pwgen whois stow dnsutils mbuffer"
 
 apt-get update
-apt-get --force-yes upgrade
+apt-get -y upgrade
 locale-gen ru_RU.UTF-8 ru_UA.UTF-8
 apt-get -y install git-core
 
