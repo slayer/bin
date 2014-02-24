ab77ea67569ac97b3df29ceed6e2cb62d54c9c60
Vladislav Moskovets <github@vlad.org.ua>
2014-02-24 23:39:05 +0200

    update

--- debian-install-dev-packages.sh
+++ debian-install-dev-packages.sh
@@ -6,7 +6,6 @@ apt-get update
 apt-get --force-yes upgrade
 apt-get -y install git-core
 apt-get -y install build-essential
-locale-gen ru_RU.UTF-8 ru_UA.UTF-8
 
 sudo apt-get install -y autoconf build-essential libxml2-dev libxslt-dev libmysqlclient-dev libcurl4-openssl-dev libreadline-dev libreadline6-dev libncurses-dev
 sudo apt-get install -y curl libcurl4-openssl-dev imagemagick libpq-dev libssl-dev zlib1g-dev libpcre3-dev libsqlite3-dev
