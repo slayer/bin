263ae991bee8f67c136e707c66d7e9d7e5d1c495
Vlad <devvlad@gmail.com>
2014-04-23 03:26:27 +0400

    mariadb

---
+++ debian-install-mariadb.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+sudo add-apt-repository 'deb http://mirror.1000mbps.com/mariadb/repo/10.0/ubuntu saucy main'
+
+sudo apt-get update
+sudo apt-get install mariadb-server
