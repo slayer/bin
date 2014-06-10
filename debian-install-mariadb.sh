55e2b257604663530bc6fe880bd3f876e564733c
Vladislav Moskovets <github@vlad.org.ua>
2014-06-11 02:21:30 +0400

    up

--- debian-install-mariadb.sh
+++ debian-install-mariadb.sh
@@ -5,4 +5,4 @@ sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082
 sudo add-apt-repository 'deb http://mirror.1000mbps.com/mariadb/repo/10.0/ubuntu trusty main'
 
 sudo apt-get update
-sudo apt-get install mariadb-server
+sudo apt-get install mariadb-server libmariadbclient-dev
