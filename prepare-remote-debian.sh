0e3ef9946542bb1fc20ea33bba3e4090b15c8068
Vlad <devvlad@gmail.com>
2014-07-08 00:29:22 +0400

    up

--- prepare-remote-debian.sh
+++ prepare-remote-debian.sh
@@ -3,9 +3,20 @@
 
 host=$1
 
-ssh root@${host} "apt-get install  -y git &&
-						git clone https://github.com/slayer/bin.git ~/bin;
-						git clone https://github.com/slayer/dotfiles.git ~/dotfiles &&
-							cd ~/dotfiles && git checkout -b dfm origin/dfm && ~/dotfiles/dfm;
-						git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-						"
+echo $host | grep "@" || host=root@${host}
+
+SSH_KEY="`cat ~/.ssh/vlad_rsa.pub`"
+
+echo "SSH_KEY: ${SSH_KEY}"
+
+ssh ${host} 'bash -xc "which git >/dev/null || apt-get install  -y git;
+						[ -d ~/bin ] && (cd ~/bin; git pull) || git clone https://github.com/slayer/bin.git ~/bin;
+						mkdir -p ~/.ssh;
+						grep '\'${SSH_KEY}\'' ~/.ssh/authorized_keys  || echo '\'$SSH_KEY\'' >>~/.ssh/authorized_keys;
+						[ -d ~/bin ] && ~/bin/install-dotfiles.sh ;
+
+						"'
+
+reset
+
+# [ -d ~/dotfiles ] && ( cd ~/dotfiles; git pull ) || git clone https://github.com/slayer/dotfiles.git ~/dotfiles
