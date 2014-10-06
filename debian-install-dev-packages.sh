69598932024f53ada04b0c05e5abfdda2bb0d837
Vladislav Moskovets <github@vlad.org.ua>
2014-10-07 00:47:28 +0400

    dev packages update

--- debian-install-dev-packages.sh
+++ debian-install-dev-packages.sh
@@ -7,11 +7,13 @@ apt-get --force-yes upgrade
 apt-get -y install git-core
 apt-get -y install build-essential
 
-sudo apt-get install -y autoconf build-essential libxml2-dev libxslt-dev libcurl4-openssl-dev libreadline-dev libreadline6-dev libncurses-dev
+sudo apt-get install -y autoconf build-essential libxml2-dev libxslt-dev libcurl4-openssl-dev libreadline-dev libreadline6-dev libncurses-dev libgmp-dev libgmp10
 sudo apt-get install -y curl libcurl4-openssl-dev imagemagick libpq-dev libssl-dev zlib1g-dev libpcre3-dev libsqlite3-dev
 sudo apt-get install -y strace ltrace tcpdump tcpick whois
 sudo apt-get install -y ack-grep
 sudo apt-get install -y libmysqlclient-dev # libmariadbclient-dev
 sudo apt-get install -y etckeeper
+
+# sudo apt-get install -y libmagickcore-dev libmagick++-dev imagemagick
 
 sudo apt-get clean
