7c2be5af528a30f38a5d9d43266dc2f138a9ce39
Vlad <devvlad@gmail.com>
2016-12-14 01:49:10 +0200

    debian-install-nettools.sh; dotfiles update

---
+++ debian-install-nettools.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo apt-get install connect-proxy tor tcpick tcpdump whois nmap netcat
--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -5,6 +5,5 @@ which git >/dev/null || apt-get install -y git
 
 [ -d ~/bin ] && (cd ~/bin && git pull) || git clone https://github.com/slayer/bin.git ~/bin
 git clone https://github.com/slayer/dotfiles.git ~/dotfiles && cd ~/dotfiles && ~/dotfiles/dfm
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-which vim && vim -c "BundleInstall"
+" git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# which vim && vim -c "BundleInstall"
