b9833cc254f668e6a981e88212590c822729e0cd
Vlad <devvlad@gmail.com>
2016-12-28 03:18:21 +0200

    update install-dotfiles.sh

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,6 +4,8 @@ apt-get update
 which git >/dev/null || apt-get install -y git
 
 [ -d ~/bin ] && (cd ~/bin && git pull) || git clone https://github.com/slayer/bin.git ~/bin
-git clone https://github.com/slayer/dotfiles.git ~/dotfiles && cd ~/dotfiles && ~/dotfiles/dfm
-# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# which vim && vim -c "BundleInstall"
+if [ -d ~/dotfiles ]; then
+  pushd ~/dotfiles && git pull; popd
+else
+  git clone https://github.com/slayer/dotfiles.git ~/dotfiles && cd ~/dotfiles && ~/dotfiles/dfm
+fi
