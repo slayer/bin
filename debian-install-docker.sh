c02e380aafd1abc54f30ab8993a2522aabe98295
Vlad <devvlad@gmail.com>
2016-12-29 21:44:39 +0200

    debian-install-docker.sh update

--- debian-install-docker.sh
+++ debian-install-docker.sh
@@ -7,15 +7,21 @@ sudo apt-get -y install $PACKAGES
 sudo apt-get clean
 
 
-pip install docker-compose
+sudo pip install docker-compose
 
-echo "You can wish to modify /etc/docker/daemon.json:"
-echo
-echo '
+echo "You may wish to modify /etc/docker/daemon.json:"
+echo 'tee -a <<END >/tmp/daemon.json
 {
   "bip": "172.17.0.1/16",
   "log-driver": "json-file",
-  "log-opts": {"max-size": "5m", "max-file": 10},
+  "log-opts": {"max-size": "5m", "max-file": "10"},
   "storage-driver": "overlay"
 }
+END2
+
+sudo systemctl stop docker
+# rm -rf /var/lib/docker
+sudo systemctl start docker
+
+sudo usermod -aG docker `whoami`
 '
