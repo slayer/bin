4097e65c7bd9c7cecf04c2d18437cd254873be15
Vladislav Moskovets <github@vlad.org.ua>
2017-05-15 19:04:30 +0300

    debian-install-docker: disable iptables

--- debian-install-docker.sh
+++ debian-install-docker.sh
@@ -15,13 +15,18 @@ echo 'tee -a <<END >/tmp/daemon.json
   "bip": "172.17.0.1/16",
   "log-driver": "json-file",
   "log-opts": {"max-size": "5m", "max-file": "10"},
-  "storage-driver": "overlay"
+  "storage-driver": "overlay",
+  "iptables": false
 }
-END2
+END
 
+# sudo mv /tmp/daemon.json /etc/docker/
 sudo systemctl stop docker
 # rm -rf /var/lib/docker
 sudo systemctl start docker
 
 sudo usermod -aG docker `whoami`
+
+sudo sed -i 's/DEFAULT_FORWARD_POLICY=.*/DEFAULT_FORWARD_POLICY="ACCEPT"' /etc/default/ufw
+sudo ufw reload
 '
