f479a79bc359853fa3eaf672a8d6ac0d8b078eaa
Vladislav Moskovets <github@vlad.org.ua>
2017-05-15 19:10:04 +0300

    forward-port: del action; socat example

--- forward-port.sh
+++ forward-port.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+# you can use socat instead:
+# socat tcp-listen:3080,reuseaddr,fork tcp:localhost:8000
+
 
 if [ -z "$1" -o -z "$2" ]; then
-  echo "Usage $0" port[/tcp] [localaddr]-daddr[:port]
+  echo "Usage $0" [-d] port[/tcp] [localaddr]-daddr[:port]
   echo "   ex $0" 3080 10.10.1.1-10.10.10.20:80
 	exit
 fi
 
+
+action="-I"
+[ "$1" = "-d" ] && action="-D"
 
 proto=tcp
 sudo="`which sudo`"
@@ -29,14 +35,14 @@ else
 	dport="$port"
 fi
 
-$sudo iptables -t nat -I PREROUTING -p $proto --dport $port -j DNAT --to-destination $daddr:$dport
+$sudo iptables -t nat $action PREROUTING -p $proto --dport $port -j DNAT --to-destination $daddr:$dport
 if [ "$laddr" ]; then
-	$sudo iptables -t nat -I POSTROUTING -p $proto -d $daddr --dport $dport -j SNAT --to-source $laddr
+	$sudo iptables -t nat $action POSTROUTING -p $proto -d $daddr --dport $dport -j SNAT --to-source $laddr
 else
-	$sudo iptables -t nat -I POSTROUTING -p $proto -d $daddr --dport $dport -j MASQUERADE
+	$sudo iptables -t nat $action POSTROUTING -p $proto -d $daddr --dport $dport -j MASQUERADE
 fi
-$sudo iptables -I FORWARD -p $proto -d $daddr --dport $dport -j ACCEPT
-$sudo iptables -I FORWARD -p $proto -s $daddr -j ACCEPT
+$sudo iptables $action FORWARD -p $proto -d $daddr --dport $dport -j ACCEPT
+$sudo iptables $action FORWARD -p $proto -s $daddr -j ACCEPT
 
 
 
