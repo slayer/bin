af3d27d3391b10b93e03165639fe51bfafda74cd
Vladislav Moskovets <github@vlad.org.ua>
2017-05-15 18:59:08 +0300

    dev packages up

--- debian-install-dev-packages.sh
+++ debian-install-dev-packages.sh
@@ -8,11 +8,11 @@ apt-get -y install git-core
 apt-get -y install build-essential
 
 sudo apt-get install -y autoconf build-essential libxml2-dev libxslt-dev libcurl4-openssl-dev libreadline-dev libreadline6-dev libncurses-dev libgmp-dev libgmp10
-sudo apt-get install -y curl libcurl4-openssl-dev imagemagick libpq-dev libssl-dev zlib1g-dev libpcre3-dev libsqlite3-dev bison
+sudo apt-get install -y curl libcurl4-openssl-dev imagemagick libpq-dev libssl-dev zlib1g-dev libpcre3-dev libsqlite3-dev bison jq
 sudo apt-get install -y strace ltrace tcpdump tcpick whois
 sudo apt-get install -y ack-grep
 sudo apt-get install -y libmysqlclient-dev sqlite3 libsqlite3-dev # libmariadbclient-dev
-sudo apt-get install -y etckeeper
+sudo apt-get install -y etckeeper inotify-tools entr
 
 # sudo apt-get install -y libmagickcore-dev libmagick++-dev imagemagick
 
