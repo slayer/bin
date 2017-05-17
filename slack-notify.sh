9bc54a0f71eb225bb31740d3c553d7367fecee92
Vladislav Moskovets <github@vlad.org.ua>
2017-05-17 23:57:48 +0300

    debian-install-docker ufw nat rules; slack_notify script

---
+++ slack-notify.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+message="$1"
+URL=https://hooks.slack.com/services/_CHANGE_ME_TO_YOUR_UNIQ_URL
+
+
+/usr/bin/curl \
+    -X POST \
+    -s \
+    --data-urlencode "payload={ \
+        \"channel\": \"#monit\", \
+        \"username\": \"Monit\", \
+        \"pretext\": \"`hostname` | $MONIT_DATE\", \
+        \"color\": \"danger\", \
+        \"icon_emoji\": \":ghost:\", \
+        \"text\": \"$MONIT_SERVICE - $MONIT_DESCRIPTION\" \
+    }" \
+    $URL
