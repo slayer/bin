e282da138b4e82fcc618caadf145be955bab134b
Vlad <devvlad@gmail.com>
2019-03-19 19:53:24 +0200

    debian-install-* updates

--- debian-install-nettools.sh
+++ debian-install-nettools.sh
@@ -2,3 +2,4 @@
 
 sudo apt-get install -y connect-proxy tcpick tcpdump whois nmap netcat netdiag iptraf net-tools
 sudo apt-get install -y lftp
+sudo apt-get install -y nmap fping arp-scan netdiscover
