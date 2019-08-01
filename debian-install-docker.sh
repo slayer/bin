046657ec3f7fbd1b9ba7434d97ceea97afb3836f
Vlad <devvlad@gmail.com>
2019-08-01 21:16:31 +0300

    debian-install-docker.sh

--- debian-install-docker.sh
+++ debian-install-docker.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -e
 
 
-PACKAGES="docker.io python-pip"
-
-sudo apt-get -y install $PACKAGES
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
 sudo apt-get clean
 
 sudo pip install docker-compose
+
+
 
 echo "You may wish to modify /etc/docker/daemon.json:"
 echo 'tee -a <<END >/tmp/daemon.json
