fff2e79b7f8498c742bca4db7f43539404cab391
Vlad <devvlad@gmail.com>
2024-03-18 14:29:50 +0200

    remove old stuff

--- debian-install-java7.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-add-apt-repository ppa:webupd8team/java
-apt-get update
-apt-get install oracle-java7-installer
--- debian-install-mariadb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install software-properties-common
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-sudo add-apt-repository 'deb http://mirror.1000mbps.com/mariadb/repo/10.0/ubuntu trusty main'
-
-sudo apt-get update
-sudo apt-get install mariadb-server libmariadbclient-dev
